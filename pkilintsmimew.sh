9a0a7d0ea4052c9d6715ad8530c72cd3074ca1ba
Jaime Hablutzel <hablutzel1@gmail.com>
2025-05-29 21:33:52 +0200

    Increasing the threshold to ERROR.

--- pkilintsmimew.sh
+++ pkilintsmimew.sh
@@ -3,8 +3,8 @@
 source /opt/misc/linters/pkilint/venv/bin/activate
 #source /Users/jhablutzel/Documents/certificate_linters/pkilint/venv/bin/activate
 
-# TODO return the severity back to NOTICE. It will require some way to exclude the CPS CP qualifier validation from the NOTICE level. Maybe I can create an specific exception for that check only.
-lint_cabf_smime_cert lint -d -s WARNING "$@"
+# FIXME increase the severity. It will require some way to exclude the CPS CP qualifier validation from the NOTICE level as well as a way to exclude the following WARNING: cabf.smime.german_ntr_registration_reference_not_euid (WARNING): Registration Reference is not in EUID format: "HRB 98213 B”.  Maybe I can create an specific exception for those checks only.
+lint_cabf_smime_cert lint -d -s ERROR "$@"
 
 exit_code=$?
 
